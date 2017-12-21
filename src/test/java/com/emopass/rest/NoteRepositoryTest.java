f93c59c4dfe83483ac250bfdcd7d95f61a4bf503
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-12-21 14:21:20 +0900

    Refactor tests.

--- src/test/java/com/emopass/rest/NoteRepositoryTest.java
+++ src/test/java/com/emopass/rest/NoteRepositoryTest.java
@@ -3,26 +3,45 @@ package com.emopass.rest;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class NoteRepositoryTest {
+  private static final String NOTE1 = "Open the door";
+  private static final String NOTE2 = "Close the door";
+  private static final String NOTE3 = "Go for a walk";
 
   private NoteRepository noteRepository;
 
   @Before
   public void setup() {
+    // TODO: Currently, we use in-memory repository, if we use real database, we need a mock database for testing
     noteRepository = NoteRepository.getInstance();
+    noteRepository.getNotes().clear();
   }
 
   @Test
-  public void addNote() {
-    final String newNoteContent = "Buy two eggs.";
-    final int beforeNoteCount = noteRepository.getNotes().size();
+  public void noteListShouldBeEmptyAtFirst() {
+    assertThat(noteRepository.getNotes().size(), equalTo(0));
+  }
 
-    noteRepository.addNote(newNoteContent);
-    final int afterNoteCount = noteRepository.getNotes().size();
+  @Test
+  public void noteShouldBeAddSuccessfully() {
+    noteRepository.addNote(NOTE1);
 
-    assertEquals(afterNoteCount, beforeNoteCount + 1);
-    assertEquals(noteRepository.getNotes().get(afterNoteCount - 1).getContent(), newNoteContent);
+    assertThat(noteRepository.getNotes().size(), equalTo(1));
+    assertThat(noteRepository.getNotes(), hasItem(new Note(1, NOTE1)));
+  }
+
+  @Test
+  public void shouldReturnAllNotesWhenGetNotes() {
+    noteRepository.addNote(NOTE1);
+    noteRepository.addNote(NOTE2);
+    noteRepository.addNote(NOTE3);
+
+    assertThat(noteRepository.getNotes().size(), equalTo(3));
+    assertThat(noteRepository.getNotes(), hasItem(new Note(1, NOTE1)));
+    assertThat(noteRepository.getNotes(), hasItem(new Note(2, NOTE2)));
+    assertThat(noteRepository.getNotes(), hasItem(new Note(3, NOTE3)));
   }
 }
