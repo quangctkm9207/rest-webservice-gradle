eb3e10d7948af3f9b0166b7c522fed1b00804cac
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-12-21 14:21:07 +0900

    Remove default notes.

--- src/main/java/com/emopass/rest/NoteRepository.java
+++ src/main/java/com/emopass/rest/NoteRepository.java
@@ -4,8 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class NoteRepository {
-  private static final Note DEFAULT_NOTE_1 = new Note(1, "Open the door");
-  private static final Note DEFAULT_NOTE_2 = new Note(2, "Close the door");
 
   private List<Note> nodes;
 
@@ -13,9 +11,6 @@ public class NoteRepository {
 
   private NoteRepository() {
     nodes = new LinkedList<>();
-    // Add default data
-    nodes.add(DEFAULT_NOTE_1);
-    nodes.add(DEFAULT_NOTE_2);
   }
 
   public static NoteRepository getInstance() {
